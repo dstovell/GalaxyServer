79644dd13431aea804c67cfae2e166972d6435a2
dstovell <ninja@Daniels-iMac.local>
2017-08-02 22:07:27 -0700

    New look

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ exports.init = function (port) {
     // view engine setup
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
-    app.set('layout', 'layout');
+    app.set('layout', 'layout_new');
 
     app.use(expressLayouts);
     app.use(favicon());
